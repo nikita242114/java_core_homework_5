58607feec5550a60d97f45fb21e3bddf980f8ba4
nikita242114 <5358711@mail.ru>
2024-03-24 21:48:36 +0500

    workin

---
+++ src/main/java/ru/ReadNumbersFromFile.java
@@ -0,0 +1,19 @@
+package ru;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ReadNumbersFromFile {
+    public static void main(String[] args) {
+        try (InputStream is = new FileInputStream("numbers.bin")) {
+            int data;
+
+            while ((data = is.read()) != -1) {
+                System.out.print(data + " ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
